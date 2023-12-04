d0efdb3168e28a930510bf1b45596ef291b88a12
Oladeji Anthony <oladejianthony98@gmail.com>
2023-12-04 13:42:14 +0100

    first commit

---
+++ src/App.js
@@ -0,0 +1,15 @@
+//import Navbar from './components/navbar/Navbar';
+import './App.scss';
+
+
+function App() {
+  return (
+    <div>
+      Adeola
+      {/* <Test /> */}
+
+    </div>
+  );
+}
+
+export default App;

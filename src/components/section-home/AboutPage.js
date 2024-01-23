bd36ce2bdd59825b15ca635e2cee032287c04a57
oladeji anthony <oladejianthony98@gmail.com>
2024-01-23 23:35:16 +0100

    homepage corrections

--- src/components/section-home/AboutPage.js
+++ src/components/section-home/AboutPage.js
@@ -1,7 +1,7 @@
 import React from 'react'
 import "../section-home/AboutPage.scss"
 import { Link } from 'react-router-dom'
-import { motion, useAnimation } from "framer-motion";
+import { motion} from "framer-motion";
 //import { useInView } from "react-intersection-observer";
 //import { useEffect } from "react";
 
